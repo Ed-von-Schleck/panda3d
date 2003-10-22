8bbdc398b1c01b26b1bd2df7fb745570711c0df0
panda3d-team <panda3d-team>
2003-10-22 04:18:20 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.59.2'.

--- direct/src/extensions/VBase3-extensions.py
+++
@@ -1,9 +0,0 @@
-
-    """
-    VBase3-extensions module: contains methods to extend functionality
-    of the VBase3 class
-    """
-
-    def __repr__(self):
-        return '%s(%s,%s,%s)' % (
-            self.__class__.__name__,self[0],self[1],self[2])
