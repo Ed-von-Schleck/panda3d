688e5b6d1c72315bc76d8302609aef33a19e5ed4
panda3d-team <panda3d-team>
2003-10-22 04:18:18 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.23.2'.

--- direct/src/particles/ParticleManagerGlobal.py
+++
@@ -1,4 +0,0 @@
-"""ParticleManagerGlobal module: contains the global particle system manager"""
-import ParticleSystemManager
-
-particleMgr = ParticleSystemManager.ParticleSystemManager()
--- dtool/src/build/Sources.pp
+++
@@ -1,8 +0,0 @@
-#define INSTALL_HEADERS \
-    ctinstmake.pl ctproj.pl ctutils.pl
-
-#define INSTALL_SCRIPTS \
-    ctaddpkg ctaddtgt ctinitproj ctproj ctpathadjust
-
-#define EXTRA_DIST \
-    initialize
