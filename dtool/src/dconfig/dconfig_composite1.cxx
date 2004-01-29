54a90be1bb459e6fb32026f78ea90e8c4b3077a4
panda3d-team <panda3d-team>
2004-01-29 03:15:32 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.2'.

--- dtool/src/dconfig/dconfig_composite1.cxx
+++
@@ -1,9 +0,0 @@
-
-#include"config_dconfig.cxx"
-#include"config_notify.cxx"
-#include"dconfig.cxx"
-#include"configTable.cxx"
-#include"notify.cxx"
-#include"notifyCategory.cxx"             
-#include"notifySeverity.cxx"
-#include"symbolEnt.cxx"
