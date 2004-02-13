4a46bd9864b0b6c82a2b6cfa379399d71e702e61
panda3d-team <panda3d-team>
2004-02-13 19:28:18 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.22'.

--- direct/src/leveleditor/Sources.pp
+++
@@ -1,3 +0,0 @@
-
-// Install scripts for building zipfiles (leveleditor and RobotToonManager)
-#define INSTALL_SCRIPTS printdir printlib copyfiles zipfiles
--- dtool/src/dtoolutil/dtoolutil_composite2.cxx
+++
@@ -1,6 +0,0 @@
-
-#include "executionEnvironment.cxx"
-#include "vector_string.cxx"
-#include "pfstream.cxx"
-#include "pfstreamBuf.cxx"
-
