1152ae1606f64cd497f737de1aa402660cfeeaec
panda3d-team <panda3d-team>
2004-02-13 19:28:19 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.86'.

--- direct/src/showbase/InputStateGlobal.py
+++
@@ -1,8 +0,0 @@
-"""instantiate global InputState object"""
-
-# This file had to be separated from MessengerGlobal to resolve a
-# circular include dependency with DirectObject.
-
-import InputState
-
-inputState = InputState.InputState()
--- panda/src/net/net_composite2.cxx
+++
@@ -1,7 +0,0 @@
-
-#include "netAddress.cxx"
-#include "netDatagram.cxx"
-#include "queuedConnectionListener.cxx"
-#include "queuedConnectionManager.cxx"
-#include "queuedConnectionReader.cxx"
-#include "recentConnectionReader.cxx"
