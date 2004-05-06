7e5cb91f4a6da99beb21bef7b92178363cdbc197
dranalli <dranalli>
2004-05-06 01:34:49 +0000

    moved smooth posHprBroadcast to smoothNodeBase

--- direct/src/distributed/DistributedSmoothNodeBase.py
+++ direct/src/distributed/DistributedSmoothNodeBase.py
@@ -1,6 +1,7 @@
 """DistributedSmoothNodeBase module: contains the DistributedSmoothNodeBase class"""
 
 from ClockDelta import *
+import Task
 
 class DistributedSmoothNodeBase:
     """common base class for DistributedSmoothNode and DistributedSmoothNodeAI
@@ -50,3 +51,125 @@ class DistributedSmoothNodeBase:
         self.clearSmoothing()
     def d_clearSmoothing(self):
         self.sendUpdate("clearSmoothing", [0])
+
+    ### posHprBroadcast ###
+
+    def getPosHprBroadcastTaskName(self):
+        # presumably, we have a doId at this point
+        return "sendPosHpr-%s" % self.doId
+
+    def stopPosHprBroadcast(self):
+        taskMgr.remove(self.getPosHprBroadcastTaskName())
+
+    def startPosHprBroadcast(self, frequency=.2):
+        taskName = self.getPosHprBroadcastTaskName()
+        # Set up telemetry optimization variables
+        xyz = self.getPos()
+        hpr = self.getHpr()
+
+        self.__storeX = xyz[0]
+        self.__storeY = xyz[1]
+        self.__storeZ = xyz[2]
+        self.__storeH = hpr[0]
+        self.__storeP = hpr[1]
+        self.__storeR = hpr[2]
+        self.__storeStop = 0
+        self.__epsilon = 0.01
+        self.__broadcastFrequency = frequency
+        # Broadcast our initial position
+        self.b_clearSmoothing()
+        self.d_setSmPosHpr(self.__storeX, self.__storeY, self.__storeZ,
+                           self.__storeH, self.__storeP, self.__storeR)
+        # remove any old tasks
+        taskMgr.remove(taskName)
+        # spawn the new task
+        taskMgr.doMethodLater(self.__broadcastFrequency, self.posHprBroadcast, taskName)
+
+    def posHprBroadcast(self, task):
+        self.d_broadcastPosHpr()
+        taskName = self.taskName("sendPosHpr")
+        taskMgr.doMethodLater(self.__broadcastFrequency,
+                              self.posHprBroadcast, taskName)
+        return Task.done
+
+    def d_broadcastPosHpr(self):
+        # send out the minimal bits to describe our new position
+        xyz = self.getPos()
+        hpr = self.getHpr()
+
+        if abs(self.__storeX - xyz[0]) > self.__epsilon:
+            self.__storeX = xyz[0]
+            newX = 1
+        else:
+            newX = 0
+
+        if abs(self.__storeY - xyz[1]) > self.__epsilon:
+            self.__storeY = xyz[1]
+            newY = 1
+        else:
+            newY = 0
+
+        if abs(self.__storeZ - xyz[2]) > self.__epsilon:
+            self.__storeZ = xyz[2]
+            newZ = 1
+        else:
+            newZ = 0
+
+        if abs(self.__storeH - hpr[0]) > self.__epsilon:
+            self.__storeH = hpr[0]
+            newH = 1
+        else:
+            newH = 0
+
+        if abs(self.__storeP - hpr[1]) > self.__epsilon:
+            self.__storeP = hpr[1]
+            newP = 1
+        else:
+            newP = 0
+
+        if abs(self.__storeR - hpr[2]) > self.__epsilon:
+            self.__storeR = hpr[2]
+            newR = 1
+        else:
+            newR = 0
+
+        # Check for changes:
+        if not(newX or newY or newZ or newH or newP or newR):
+            # No change
+            # Send one and only one "stop" message.
+            if not self.__storeStop:
+                self.__storeStop = 1
+                self.d_setSmStop()
+            # print 'no change'
+        elif (newH) and not(newX or newY or newZ or newP or newR):
+            # Only change in H
+            self.__storeStop = 0
+            self.d_setSmH(self.__storeH)
+            # print ("H change")
+        elif (newX or newY) and not(newZ or newH or newP or newR):
+            # Only change in X, Y
+            self.__storeStop = 0
+            self.d_setSmXY(self.__storeX, self.__storeY)
+            # print ("XY change")
+        elif (newX or newY or newZ) and not(newH or newP or newR):
+            # Only change in X, Y, Z
+            self.__storeStop = 0
+            self.d_setSmPos(self.__storeX, self.__storeY, self.__storeZ)
+            # print ("XYZ change")
+        elif (newX or newY or newH) and not(newZ or newP or newR):
+            # Only change in X, Y, H
+            self.__storeStop = 0
+            self.d_setSmXYH(self.__storeX, self.__storeY, self.__storeH)
+            # print ("XYH change")
+        elif (newX or newY or newZ or newH) and not(newP or newR):
+            # Only change in X, Y, Z, H
+            self.__storeStop = 0
+            self.d_setSmXYZH(self.__storeX, self.__storeY, self.__storeZ, self.__storeH)
+            # print ("XYZH change")
+        else:
+            # Other changes
+            self.__storeStop = 0
+            self.d_setSmPosHpr(self.__storeX, self.__storeY, self.__storeZ,
+                               self.__storeH, self.__storeP, self.__storeR)
+            # print ("XYZHPR change")
+
