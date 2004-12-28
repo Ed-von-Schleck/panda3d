f95df441dfb4a828f6a89e9ed57e8ccfb346c9d4
panda3d-team <panda3d-team>
2004-12-28 04:04:48 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.101.2'.

--- panda/src/pgui/pgui_composite2.cxx
+++
@@ -1,9 +0,0 @@
-
-#include "pgFrameStyle.cxx"
-#include "pgItem.cxx"
-#include "pgMouseWatcherBackground.cxx"
-#include "pgMouseWatcherRegion.cxx"
-#include "pgTop.cxx"
-#include "pgWaitBar.cxx"
-#include "pgSliderBar.cxx"
-
