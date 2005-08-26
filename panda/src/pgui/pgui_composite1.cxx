a1132abda5fe10e02613778081f493515ab416df
panda3d-team <panda3d-team>
2005-08-26 06:22:31 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.51.4.2.2'.

--- panda/src/pgui/pgui_composite1.cxx
+++
@@ -1,9 +0,0 @@
-    
-#include "config_pgui.cxx"
-#include "pgButton.cxx"
-#include "pgSliderButton.cxx"
-#include "pgCullTraverser.cxx"
-#include "pgEntry.cxx"
-#include "pgMouseWatcherGroup.cxx"
-#include "pgMouseWatcherParameter.cxx"
-
