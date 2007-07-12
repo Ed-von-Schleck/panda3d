15d542f1435c20c1da7cdfa2c437fae3514a0128
panda3d-team <panda3d-team>
2007-07-12 20:54:39 +0000

    This commit was manufactured by cvs2svn to create branch 'pirates_1_0_10_13b'.

--- panda/src/audiotraits/miles_audio_composite1.cxx
+++
@@ -1,5 +0,0 @@
-
-#include "config_milesAudio.cxx"
-#include "milesAudioManager.cxx"
-#include "milesAudioSound.cxx"
-      
--- panda/src/movies/movies_composite1.cxx
+++
@@ -1,4 +0,0 @@
-#include "movieVideo.cxx"
-#include "movieAudio.cxx"
-#include "movie.cxx"
-#include "config_movies.cxx"
