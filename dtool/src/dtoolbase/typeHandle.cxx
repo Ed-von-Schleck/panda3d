0bcefeec3952e510c98a8849099d0b997b7c0b37
chico-san <chico-san>
2007-08-09 00:36:19 +0000

    commented out the other cerr statement as to not spam AI/Uberdog logs

--- dtool/src/dtoolbase/typeHandle.cxx
+++ dtool/src/dtoolbase/typeHandle.cxx
@@ -80,7 +80,7 @@ dec_memory_usage(MemoryClass memory_class, int size) {
     assert(rnode != (TypeRegistryNode *)NULL);
     AtomicAdjust::add(rnode->_memory_usage[memory_class], -(PN_int32)size);
     if (rnode->_memory_usage[memory_class] < 0) {
-      cerr << *this << ".dec(" << memory_class << ", " << size << ") -> " << rnode->_memory_usage[memory_class] << "\n";
+    //  cerr << *this << ".dec(" << memory_class << ", " << size << ") -> " << rnode->_memory_usage[memory_class] << "\n";
     }
     // assert(rnode->_memory_usage[memory_class] >= 0);
   }
