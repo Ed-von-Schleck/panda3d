b588ea2ac1374049e95597882d84118be1498a08
chico-san <chico-san>
2008-08-13 19:20:03 +0000

    MFC: fix for inverted screenshot (1.114) - drwr

--- panda/src/dxgsg8/dxGraphicsStateGuardian8.h
+++ panda/src/dxgsg8/dxGraphicsStateGuardian8.h
@@ -102,6 +102,8 @@ public:
                                            const RenderBuffer &rb);
   virtual bool framebuffer_copy_to_ram(Texture *tex, int z, const DisplayRegion *dr,
                                        const RenderBuffer &rb);
+  bool do_framebuffer_copy_to_ram(Texture *tex, int z, const DisplayRegion *dr,
+                                  const RenderBuffer &rb, bool inverted);
 
   virtual void reset();
 
