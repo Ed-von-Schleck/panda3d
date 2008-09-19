b38005831b0963b8c95dab312c07ac620a5ca85e
panda3d-team <panda3d-team>
2008-09-19 00:28:06 +0000

    This commit was manufactured by cvs2svn to create branch 'pirates_1_0_12_7b'.

--- dtool/src/dtoolutil/dtoolutil_composite2.cxx
+++
@@ -1,7 +0,0 @@
-#include "pandaFileStream.cxx"
-#include "pandaFileStreamBuf.cxx"
-#include "executionEnvironment.cxx"
-#include "vector_string.cxx"
-#include "pfstream.cxx"
-#include "pfstreamBuf.cxx"
-
