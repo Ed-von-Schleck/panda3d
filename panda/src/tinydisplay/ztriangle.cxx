7d0029070968ef4c5a8d3e438729c52654d9aacb
panda3d-team <panda3d-team>
2008-11-06 17:20:11 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.19.14'.

--- panda/src/tinydisplay/ztriangle.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "zbuffer.h"
-
-/* Pick up all of the generated code references to ztriangle_two.h,
-   which ultimately calls ztriangle.h, many, many times. */
-
-#include "ztriangle_table.h"
-#include "ztriangle_code.h"
