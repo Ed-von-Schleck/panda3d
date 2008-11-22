273eb9ca3dd6316ebd02ffc33a1951a0b8471712
hatjosh <hatjosh>
2008-11-22 02:24:27 +0000

    enabling '-exclude' option to maya2egg

--- pandatool/src/mayaprogs/mayaToEgg.cxx
+++ pandatool/src/mayaprogs/mayaToEgg.cxx
@@ -4,15 +4,11 @@
 ////////////////////////////////////////////////////////////////////
 //
 // PANDA 3D SOFTWARE
-// Copyright (c) 2001 - 2004, Disney Enterprises, Inc.  All rights reserved
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
-// All use of this software is subject to the terms of the Panda 3d
-// Software license.  You should have received a copy of this license
-// along with this source code; you will also find a current copy of
-// the license at http://etc.cmu.edu/panda3d/docs/license/ .
-//
-// To contact the maintainers of this program write to
-// panda3d-general@lists.sourceforge.net .
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
 //
 ////////////////////////////////////////////////////////////////////
 
@@ -118,6 +114,15 @@ MayaToEgg() :
      &MayaToEgg::dispatch_vector_string, NULL, &_subsets);
 
   add_option
+    ("exclude", "name", 0,
+     "Specifies that a subset of the geometry in the Maya file should "
+     "not be converted; specifically, the geometry under the node or nodes whose "
+     "name matches the parameter (which may include globbing characters "
+     "like * or ?).  This parameter may be repeated multiple times to name "
+     "multiple roots.",
+     &MayaToEgg::dispatch_vector_string, NULL, &_excludes);
+
+  add_option
     ("ignore-slider", "name", 0,
      "Specifies the name of a slider (blend shape deformer) that maya2egg "
      "should not process.  The slider will not be touched during conversion "
@@ -200,11 +205,17 @@ run() {
     }
   }
 
-  //vector_string::const_iterator si;
   if (!_subsets.empty()) {
     converter.clear_subsets();
     for (si = _subsets.begin(); si != _subsets.end(); ++si) {
       converter.add_subset(GlobPattern(*si));
+    }
+  }
+
+  if (!_excludes.empty()) {
+    converter.clear_excludes();
+    for (si = _excludes.begin(); si != _excludes.end(); ++si) {
+      converter.add_exclude(GlobPattern(*si));
     }
   }
 
