9f4931e7e274dd276c7146990c84dcbf38f7aedc
panda3d-team <panda3d-team>
2009-01-31 17:24:38 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.22.8'.

--- panda/src/grutil/grutil_composite2.cxx
+++
@@ -1,7 +0,0 @@
-#include "openCVTexture.cxx"
-#include "ffmpegTexture.cxx"
-#include "movieTexture.cxx"
-#include "nodeVertexTransform.cxx"
-#include "pipeOcclusionCullTraverser.cxx"
-#include "rigidBodyCombiner.cxx"
-#include "meshDrawer.cxx"
