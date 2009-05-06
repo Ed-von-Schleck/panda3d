e2513b2d90a9903c0e1b368d1d3723b13b0ca2bf
hatjosh <hatjosh>
2009-05-06 17:29:53 +0000

    pulling in 1.387:1.388. (PC-2095) addendum code to the cannon timing rework

--- direct/src/showbase/ShowBase.py
+++ direct/src/showbase/ShowBase.py
@@ -810,7 +810,6 @@ class ShowBase(DirectObject.DirectObject):
 
             self.setFrameRateMeter(self.config.GetBool(
                 'show-frame-rate-meter', 0))
-
         return success
 
     def setSleep(self, amount):
@@ -2408,6 +2407,9 @@ class ShowBase(DirectObject.DirectObject):
             base.direct.enable()
         else:
             __builtin__.direct = self.direct = None
+
+    def getRepository(self):
+        return None
 
     def __doStartDirect(self):
         if self.__directStarted:
