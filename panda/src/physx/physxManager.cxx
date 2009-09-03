86063ba2c3b9c1f1e7da92d062bb1c5718f846a1
enn0x <enn0x>
2009-09-03 19:59:48 +0000

    Initialize PhysX SDK

--- panda/src/physx/physxManager.cxx
+++ panda/src/physx/physxManager.cxx
@@ -14,10 +14,12 @@
 
 #include "physxManager.h"
 
+PhysxManager::PhysxOutputStream PhysxManager::_outputStream;
+
 ////////////////////////////////////////////////////////////////////
 //     Function : PhysxManager
 //       Access : Published
-//  Description :
+//  Description : 
 ////////////////////////////////////////////////////////////////////
 PhysxManager::
 PhysxManager() {
@@ -25,19 +27,108 @@ PhysxManager() {
   NxSDKCreateError error;
   NxPhysicsSDKDesc desc = NxPhysicsSDKDesc();
 
-  _sdk = NxCreatePhysicsSDK( NX_PHYSICS_SDK_VERSION, NULL, NULL, desc, &error );
+  _sdk = NxCreatePhysicsSDK( NX_PHYSICS_SDK_VERSION, NULL, &_outputStream, desc, &error );
 
-  cout << "----> " << _sdk << "\n"; // TODO
+  if ( error == NXCE_NO_ERROR ) {
+    physx_cat.info() << "PhysX subsystem initialized. Number of PPUs="
+                     << _sdk->getNbPPUs() << endl;
+  }
+  else {
+    physx_cat.error() << "Error when setting up the PhysX subsystem: "
+                      << get_sdk_error_string(error) << endl;
+  }
+
+  nassertv_always( error == NXCE_NO_ERROR );
+  nassertv_always( _sdk );
 }
 
 ////////////////////////////////////////////////////////////////////
 //     Function : ~PhysxManager
 //       Access : Published
-//  Description :
+//  Description : 
 ////////////////////////////////////////////////////////////////////
 PhysxManager::
 ~PhysxManager() {
 
   NxReleasePhysicsSDK( _sdk );
+}
+
+////////////////////////////////////////////////////////////////////
+//     Function: PhysxManager::get_sdk_error_string
+//       Access: Private
+//  Description: Returns the NxSDKCreateError enum as string.
+////////////////////////////////////////////////////////////////////
+const char *PhysxManager::
+get_sdk_error_string(const NxSDKCreateError &error) {
+
+  switch ( error ) {
+  case NXCE_NO_ERROR:           return "NXCE_NO_ERROR"; break;
+  case NXCE_PHYSX_NOT_FOUND:    return "NXCE_PHYSX_NOT_FOUND"; break;
+  case NXCE_WRONG_VERSION:      return "NXCE_WRONG_VERSION"; break;
+  case NXCE_DESCRIPTOR_INVALID: return "NXCE_DESCRIPTOR_INVALID"; break;
+  case NXCE_CONNECTION_ERROR:   return "NXCE_CONNECTION_ERROR"; break;
+  case NXCE_RESET_ERROR:        return "NXCE_RESET_ERROR"; break;
+  case NXCE_IN_USE_ERROR:       return "NXCE_IN_USE_ERROR"; break;
+  case NXCE_BUNDLE_ERROR:       return "NXCE_BUNDLE_ERROR"; break;
+  default:                      return "Unknown error"; break;
+  }
+}
+
+////////////////////////////////////////////////////////////////////
+//     Function : PhysxOutputStream::reportError
+//       Access : Private
+//  Description : Reports an error code from the PhysX SDK.
+////////////////////////////////////////////////////////////////////
+void PhysxManager::PhysxOutputStream::
+reportError(NxErrorCode code, const char *message, const char *file, int line) {
+
+  physx_cat.error() << get_error_code_string(code) << ": " 
+                    << message << endl;
+}
+
+////////////////////////////////////////////////////////////////////
+//     Function : PhysxOutputStream::get_error_code_string
+//       Access : Private
+//  Description : Returns the NxSDKCreateError enum as string.
+////////////////////////////////////////////////////////////////////
+const char *PhysxManager::PhysxOutputStream::
+get_error_code_string(NxErrorCode code) {
+  
+  switch (code) {
+  case NXE_NO_ERROR:           return "NO_ERROR"; break;
+  case NXE_INVALID_PARAMETER:  return "INVALID_PARAMETER"; break;
+  case NXE_INVALID_OPERATION:  return "INVALID_OPERATION"; break;
+  case NXE_OUT_OF_MEMORY:      return "OUT_OF_MEMORY"; break;
+  case NXE_INTERNAL_ERROR:     return "INTERNAL_ERROR"; break;
+  case NXE_ASSERTION:          return "ASSERTION"; break;
+  case NXE_DB_INFO:            return "DB_INFO"; break;
+  case NXE_DB_WARNING:         return "DB_WARNING"; break;
+  case NXE_DB_PRINT:           return "DB_PRINT"; break;
+  }
+}
+
+////////////////////////////////////////////////////////////////////
+//     Function : PhysxOutputStream::reportAssertViolation
+//       Access : Private
+//  Description : Reports an assertion violation from the PhysX
+//                SDK.
+////////////////////////////////////////////////////////////////////
+NxAssertResponse PhysxManager::PhysxOutputStream::
+reportAssertViolation(const char *message, const char *file, int line) {
+
+  physx_cat.error() << "AssertViolation: " << message << endl;
+
+  return NX_AR_CONTINUE;
+}
+
+////////////////////////////////////////////////////////////////////
+//     Function : PhysxOutputStream::print
+//       Access : Private
+//  Description : Prints some debug text from the PhysX SDK.
+////////////////////////////////////////////////////////////////////
+void PhysxManager::PhysxOutputStream::
+print(const char *message) {
+
+  nout << message;
 }
 
--- panda/src/physx/physxManager.h
+++ panda/src/physx/physxManager.h
@@ -24,7 +24,9 @@
 
 ////////////////////////////////////////////////////////////////////
 //       Class : PhysxManager
-// Description : TODO
+// Description : The central interface to the PhysX subsystem.
+//               Used e. g. for setting/retrieving global parameters
+//               or for creating scenes.
 ////////////////////////////////////////////////////////////////////
 class EXPCL_PANDAPHYSX PhysxManager {
 
@@ -35,6 +37,15 @@ PUBLISHED:
 private:
   NxPhysicsSDK *_sdk;
 
+  class PhysxOutputStream : public NxUserOutputStream {
+    void reportError(NxErrorCode code, const char *message, const char *file, int line);
+    NxAssertResponse reportAssertViolation(const char *message, const char *file, int line);
+    void print(const char *message);
+    const char *get_error_code_string(NxErrorCode code);
+  };
+  static PhysxOutputStream _outputStream;
+
+  static const char *get_sdk_error_string(const NxSDKCreateError &error);
 };
 
 #include "physxManager.I"
