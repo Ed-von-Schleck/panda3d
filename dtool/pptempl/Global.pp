4c611aef0ea166decb4e405f29c3306fca60f977
chico-san <chico-san>
2009-12-24 22:57:59 +0000

    MVC: integrating awesomium with panda (1.147) - rurbino

--- dtool/pptempl/Global.pp
+++ dtool/pptempl/Global.pp
@@ -198,6 +198,13 @@
   #define ode_libs $[ODE_LIBS]
 #endif
 
+#if $[HAVE_AWESOMIUM]
+  #define awesomium_ipath $[wildcard $[AWESOMIUM_IPATH]]
+  #define awesomium_lpath $[wildcard $[AWESOMIUM_LPATH]]
+  #define awesomium_libs $[AWESOMIUM_LIBS]
+  #define awesomium_framework $[AWESOMIUM_FRAMEWORK]
+#endif
+
 #if $[HAVE_JPEG]
   #define jpeg_ipath $[wildcard $[JPEG_IPATH]]
   #define jpeg_lpath $[wildcard $[JPEG_LPATH]]
