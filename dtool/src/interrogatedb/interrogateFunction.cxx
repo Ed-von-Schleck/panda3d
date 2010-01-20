e6a7cd090d58605f93b3ca6641491ce7b227f5fb
sechoi <sechoi>
2010-01-20 18:19:00 +0000

    [int'l] msvc7

--- dtool/src/interrogatedb/interrogateFunction.cxx
+++ dtool/src/interrogatedb/interrogateFunction.cxx
@@ -4,11 +4,15 @@
 ////////////////////////////////////////////////////////////////////
 //
 // PANDA 3D SOFTWARE
-// Copyright (c) Carnegie Mellon University.  All rights reserved.
+// Copyright (c) 2001 - 2004, Disney Enterprises, Inc.  All rights reserved
 //
-// All use of this software is subject to the terms of the revised BSD
-// license.  You should have received a copy of this license along
-// with this source code in a file named "LICENSE."
+// All use of this software is subject to the terms of the Panda 3d
+// Software license.  You should have received a copy of this license
+// along with this source code; you will also find a current copy of
+// the license at http://etc.cmu.edu/panda3d/docs/license/ .
+//
+// To contact the maintainers of this program write to
+// panda3d-general@lists.sourceforge.net .
 //
 ////////////////////////////////////////////////////////////////////
 
@@ -16,30 +20,6 @@
 #include "indexRemapper.h"
 #include "interrogate_datafile.h"
 #include "interrogateDatabase.h"
-
-////////////////////////////////////////////////////////////////////
-//     Function: InterrogateFunction::Constructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-InterrogateFunction::
-InterrogateFunction(InterrogateModuleDef *def) :
-  InterrogateComponent(def)
-{
-  _flags = 0;
-  _class = 0;
-  _instances = (Instances *)NULL;
-}
-
-////////////////////////////////////////////////////////////////////
-//     Function: InterrogateFunction::Copy Constructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-InterrogateFunction::
-InterrogateFunction(const InterrogateFunction &copy) {
-  (*this) = copy;
-}
 
 ////////////////////////////////////////////////////////////////////
 //     Function: InterrogateFunction::Copy Assignment Operator
