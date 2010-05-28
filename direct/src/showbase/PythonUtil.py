3eb1a35f9d57ba6eb7b1fee2e28a8d468ba42f20
hatjosh <hatjosh>
2010-05-28 21:40:37 +0000

    merging in 'HEAD_to_teleport:HEAD'

--- direct/src/showbase/PythonUtil.py
+++ direct/src/showbase/PythonUtil.py
@@ -4328,7 +4328,7 @@ def bpdbGetEnabled():
     finally:
         return enabled
 bpdb.setEnabledCallback(bpdbGetEnabled)
-bpdb.setConfigCallback(lambda cfg: ConfigVariableBool('want-bp-%s' % (cfg,), 0).getValue())
+bpdb.setConfigCallback(lambda cfg: ConfigVariableBool('want-bp-%s' % (cfg.lower(),), 0).getValue())
 
 
 import __builtin__
