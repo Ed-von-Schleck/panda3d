6993bb697e97151f2022a1bdd890c4a202bb30ad
pro-rsoft <pro-rsoft>
2010-07-20 12:47:12 +0000

    I don't get paid enough to write elaborate commit messages

---
+++ panda/src/navigation/recastNavMesh.cxx
@@ -0,0 +1,17 @@
+// Filename: recastNavMesh.cxx
+// Created by:  rdb (20Jul10)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
+//
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+////////////////////////////////////////////////////////////////////
+
+#include "recastNavMesh.h"
+
+
---
+++ panda/src/navigation/recastNavMesh.h
@@ -0,0 +1,28 @@
+// Filename: recastNavMesh.h
+// Created by:  rdb (20Jul10)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
+//
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+////////////////////////////////////////////////////////////////////
+
+#ifndef RECASTNAVMESH_H
+#define RECASTNAVMESH_H
+
+////////////////////////////////////////////////////////////////////
+//       Class : RecastNavMesh
+// Description :
+////////////////////////////////////////////////////////////////////
+class RecastNavMesh {
+};
+
+#include "recastNavMesh.I"
+
+#endif
+
