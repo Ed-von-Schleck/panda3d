36b904fdb9d7342e8f368876fd3495d5e0893ca9
panda3d-team <panda3d-team>
2010-07-31 00:43:41 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.2.2.2'.

--- panda/src/vision/vision_composite1.cxx
+++
@@ -1,8 +0,0 @@
-#include "arToolKit.cxx"
-#include "config_vision.cxx"
-#include "openCVTexture.cxx"
-#include "webcamVideo.cxx"
-#include "webcamVideoCursorV4L.cxx"
-#include "webcamVideoDS.cxx"
-#include "webcamVideoV4L.cxx"
-
