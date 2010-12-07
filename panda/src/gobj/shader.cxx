218e7dd172d3dd4f9c96cc1860895d7d61db6145
pro-rsoft <pro-rsoft>
2010-12-07 21:10:12 +0000

    support generation 5 gpu programs

--- panda/src/gobj/shader.cxx
+++ panda/src/gobj/shader.cxx
@@ -1651,7 +1651,10 @@ Shader(CPT(ShaderFile) filename, CPT(ShaderFile) text, const ShaderLanguage &lan
     _default_caps._active_fprofile = CG_PROFILE_UNKNOWN;
     _default_caps._ultimate_vprofile = cgGetProfile("glslv");
     _default_caps._ultimate_fprofile = cgGetProfile("glslf");
-    _default_caps._ultimate_gprofile = cgGetProfile("gp4gp");
+    _default_caps._ultimate_gprofile = cgGetProfile("glslg");
+    if (_default_caps._ultimate_gprofile == CG_PROFILE_UNKNOWN) {
+      _default_caps._ultimate_gprofile = cgGetProfile("gp4gp");
+    }
   }
 #endif
   
@@ -1727,6 +1730,9 @@ cg_get_profile_from_header(ShaderCaps& caps) {
       if ((int)buf.find("gp4vp") >= 0)
         caps._active_vprofile = cgGetProfile("gp4vp");
 
+      if ((int)buf.find("gp5vp") >= 0)
+        caps._active_vprofile = cgGetProfile("gp5vp");
+
       if ((int)buf.find("glslv") >= 0)
         caps._active_vprofile = cgGetProfile("glslv");
 
@@ -1757,9 +1763,15 @@ cg_get_profile_from_header(ShaderCaps& caps) {
       if ((int)buf.find("vs_4_0") >= 0)
         caps._active_vprofile = cgGetProfile("vs_4_0");
 
+      if ((int)buf.find("vs_5_0") >= 0)
+        caps._active_vprofile = cgGetProfile("vs_5_0");
+
       // Scan the line for known cg2 fragment program profiles
       if ((int)buf.find("gp4fp") >= 0)
         caps._active_fprofile = cgGetProfile("gp4fp");
+
+      if ((int)buf.find("gp5fp") >= 0)
+        caps._active_fprofile = cgGetProfile("gp5fp");
 
       if ((int)buf.find("glslf") >= 0)
         caps._active_fprofile = cgGetProfile("glslf");
@@ -1797,15 +1809,24 @@ cg_get_profile_from_header(ShaderCaps& caps) {
       if ((int)buf.find("ps_4_0") >= 0)
         caps._active_fprofile = cgGetProfile("ps_4_0");
 
+      if ((int)buf.find("ps_5_0") >= 0)
+        caps._active_fprofile = cgGetProfile("ps_5_0");
+
       // Scan the line for known cg2 geometry program profiles
       if ((int)buf.find("gp4gp") >= 0)
         caps._active_gprofile = cgGetProfile("gp4gp");
+
+      if ((int)buf.find("gp5gp") >= 0)
+        caps._active_gprofile = cgGetProfile("gp5gp");
 
       if ((int)buf.find("glslg") >= 0)
         caps._active_gprofile = cgGetProfile("glslg");
 
       if ((int)buf.find("gs_4_0") >= 0)
         caps._active_gprofile = cgGetProfile("gs_4_0");
+
+      if ((int)buf.find("gs_5_0") >= 0)
+        caps._active_gprofile = cgGetProfile("gs_5_0");
     }
   } while(_parse > lastParse);
 
