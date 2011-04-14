986602cf91ebb2a69493734d103094f2d9f34bf3
thekrush <thekrush>
2011-04-14 08:21:42 +0000

    - forgot to add this file with my previous commit

---
+++ panda/src/cegui/cegui_includes.h
@@ -0,0 +1,39 @@
+
+#ifndef CEGUI_INCLUDES_H
+#define CEGUI_INCLUDES_H
+
+#undef min
+#undef max
+
+#include "CEGUI.h"
+#include "CEGUIBase.h"
+#include "CEGUIDefaultResourceProvider.h"
+#include "CEGUIExceptions.h"
+#include "CEGUIFactoryModule.h"
+#include "CEGUIFontManager.h"
+#include "CEGUIGeometryBuffer.h"
+#include "CEGUIImageCodec.h"
+#include "CEGUIImageset.h"
+#include "CEGUIImagesetManager.h"
+#include "CEGUIInputEvent.h"
+#include "CEGUIRect.h"
+#include "CEGUIRenderEffect.h"
+#include "CEGUIRenderer.h"
+#include "CEGUIRenderingRoot.h"
+#include "CEGUIRenderQueue.h"
+#include "CEGUIRenderTarget.h"
+#include "CEGUIScheme.h"
+#include "CEGUISchemeManager.h"
+#include "CEGUISize.h"
+#include "CEGUISystem.h"
+#include "CEGUITexture.h"
+#include "CEGUITextureTarget.h"
+#include "CEGUIVector.h"
+#include "CEGUIVertex.h"
+#include "CEGUIWindow.h"
+#include "CEGUIWindowManager.h"
+
+#include "elements/CEGUIFrameWindow.h"
+#include "falagard/CEGUIFalWidgetLookManager.h"
+
+#endif // CEGUI_INCLUDES_H
