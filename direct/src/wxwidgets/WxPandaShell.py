42196a4ba2033f8964d7214bea4da3b8f539268b
jlbutler <jlbutler>
2011-09-02 14:55:09 +0000

    merge from disney_stable fix

--- direct/src/wxwidgets/WxPandaShell.py
+++ direct/src/wxwidgets/WxPandaShell.py
@@ -223,3 +223,20 @@ class WxPandaShell(WxAppShell):
                 base.winList[i].setActive(1)
 
         self.viewFrame.SetExpanded(viewIdx)
+    
+    def getCurrentView(self):
+        """Function for get the current Viewport"""
+        if self.viewFrame._expanded == -1: #four view
+            self.currentView = None
+        if self.viewFrame._expanded == 0: #top view
+            self.currentView = self.topView
+        if self.viewFrame._expanded == 1: #front view
+            self.currentView = self.frontView
+        if self.viewFrame._expanded == 2: #left view
+            self.currentView = self.leftView
+        if self.viewFrame._expanded == 3: #perspect view
+            self.currentView = self.perspView
+            
+        return self.currentView
+        
+
