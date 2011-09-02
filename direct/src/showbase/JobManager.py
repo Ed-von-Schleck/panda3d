e664226d8a828d235a7144b02bb6c3f92ef44cb8
jlbutler <jlbutler>
2011-09-02 15:36:24 +0000

    merge from disney_stable fix

--- direct/src/showbase/JobManager.py
+++ direct/src/showbase/JobManager.py
@@ -219,6 +219,9 @@ class JobManager:
                     break
         return task.cont
 
+    def __len__(self):
+        return len(self._jobId2pri)
+
     def __repr__(self):
         s  =   '======================================================='
         s += '\nJobManager: active jobs in descending order of priority'
